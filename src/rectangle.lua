a38e0181cbd0a50de917c07b4c4e9f06e5a98fef
eniallator <eniallatormc@gmail.com>
2017-07-21 12:16:15 +1000

    Removed unecessary variable in rectangle

--- src/rectangle.lua
+++ src/rectangle.lua
@@ -6,12 +6,10 @@ local function createRectangle(x, y, w, h)
   rectangle.h = h
 
   function rectangle:getMid()
-    local mid = {
+    return {
       x = self.x + self.w / 2,
       y = self.y + self.h / 2
     }
-
-    return mid
   end
 
   return rectangle
